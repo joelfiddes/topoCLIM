46d0411ae1af42774a9c8f9881f34489c51696dd
Joel Fiddes <joelfiddes@gmail.com>
2020-07-28 17:44:58 +0200

    cleanup

--- esgf_get.py
+++ esgf_get.py
@@ -1,7 +1,18 @@
 # https://esgf-pyclient.readthedocs.io/en/latest/notebooks/demo/subset-cmip5.html
 from pyesgf.logon import LogonManager
+import os.path
+import logging
 
+#fixed
+project='CORDEX'
+domain='EUR-44'
+time_frequency='day'
 outdir="/home/joel/sim/qmap/test/pyout/"
+
+# change to logging.DEBUG to get debug indo
+logging.basicConfig(level=logging.INFO, filename=outdir+"logfile", filemode="w",
+                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+# variables
 vars=('tas', 'pr', 'ps', 'hurs', 'rsds','rlds', 'uas', 'vas')
 expers=('historical', 'rcp26', 'rcp85')
 # define domain here (once figured out rlat/lon translation):
@@ -16,26 +27,31 @@ lonend=60
 latstart=40
 latend=50
 
+# logon manager
 lm = LogonManager()
-lm.logoff()
-lm.is_logged_on()
+if not lm.is_logged_on():
 
-OPENID = 'https://esgf-data.dkrz.de/esgf-idp/openid/jfiddes'
-lm.logon_with_openid(openid=OPENID, password="sT0kkang!", bootstrap=True)
-lm.is_logged_on()
+    lm = LogonManager()
+    lm.logoff()
+    lm.is_logged_on()
 
-lm.logon(hostname='esgf-data.dkrz.de', interactive=True, bootstrap=True)
-lm.is_logged_on()
+    OPENID = 'https://esgf-data.dkrz.de/esgf-idp/openid/jfiddes'
+    lm.logon_with_openid(openid=OPENID, password=None, bootstrap=True)
+    lm.is_logged_on()
+
+    lm.logon(hostname='esgf-data.dkrz.de', interactive=True, bootstrap=True)
+    lm.is_logged_on()
 
 
 from pyesgf.search import SearchConnection
 conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True)
 
-for var in vars:
-    myvar=var
-    print("Variable: " +var)
-    for exper in expers:
-        print("Experimet: "+exper)
+for exper in expers:
+    logging.info("Experimet: "+exper)
+    for var in vars:
+        myvar=var
+        logging.info("Variable: " +var)
+
         ctx = conn.new_context(
             project='CORDEX',
             domain='EUR-44',
@@ -43,31 +59,46 @@ for var in vars:
             time_frequency='day',
             variable=var
             )
-        print("Found hits: " + str(ctx.hit_count))
+        logging.info("Found hits: " + str(ctx.hit_count))
+
+        # f = open("demofile2.txt", "a")
+        # f.write("Now the file has more content!")
+        # f.close()
 
         # loop through hits
         for hit in range(0,ctx.hit_count):
-            print("retrieving hit: " +str(hit+1)+"/"+str(ctx.hit_count) +" "+var+" "+exper)
+            logging.info("retrieving hit: " +str(hit+1)+"/"+str(ctx.hit_count) +" "+var+" "+exper)
             result = ctx.search()[hit]
             result.dataset_id
 
 
             files = result.file_context().search()
             for file in files:
-                if myvar in file.opendap_url:
-                    my_url = file.opendap_url
-                    #print(my_url)
 
-                    import xarray as xr
-                    ds = xr.open_dataset(my_url)
-                    #print(ds)
-                    #rp = ds[rotate_pole]
-                    da = ds[myvar]
-                    #da = da.sel(lat=slice(latS, latN), lon=slice(lonE, lonW))
-                    da2 = da[:,latstart:latend, lonstart:lonend]
-                    outname = my_url.split('/')[-1]
-                    da2.to_netcdf(outdir+outname)
-                    print(outname+" done!")
+                my_url = file.opendap_url
+                if my_url is None:
+                    logging.info("No URL available in hit " + str(hit))
+                    continue
+
+                outname = my_url.split('/')[-1]
+                if os.path.isfile(outdir+outname):
+                    logging.info (outname+" already downloaded!")
+                    continue
+
+
+                import xarray as xr
+                try:
+                    ds = xr.open_dataset(my_url, decode_times=False)
+                except IOError:
+                    logging.info("Server likely down skipping"+my_url)
+                #logging.info(ds)
+                #rp = ds[rotate_pole]
+                da = ds[myvar]
+                #da = da.sel(lat=slice(latS, latN), lon=slice(lonE, lonW))
+                da2 = da[:,latstart:latend, lonstart:lonend]
+
+                da2.to_netcdf(outdir+outname)
+                logging.info(outname+" done!")
 
 
 
